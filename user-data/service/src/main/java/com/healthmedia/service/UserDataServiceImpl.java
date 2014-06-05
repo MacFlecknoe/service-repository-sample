d7e5dcb17302eb49e5a2148134ea6d92eb3a9e0b
Michael Lambert <mlamber7@its.jnj.com>
2014-06-05 16:52:02 -0400

    added packagename to wadl2java

---
+++ user-data/service/src/main/java/com/healthmedia/service/UserDataServiceImpl.java
@@ -0,0 +1,26 @@
+package com.healthmedia.service;
+
+import healthmedia.schema.user_data.v1.UserDataCollectionType;
+import healthmedia.schema.user_data.v1.UserDataType;
+
+import java.util.Date;
+
+import com.healthmedia.ws.userdata.UserData;
+
+public class UserDataServiceImpl implements UserData {
+
+	public UserDataCollectionType search(String query, Date collectionDate_start, Date collectionDate_end, String data_name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserDataType create(UserDataType userdatatype) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserDataCollectionType createMultiple(UserDataCollectionType userdatacollectiontype) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
