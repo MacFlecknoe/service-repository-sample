79b4aa8828f3338b5e178ae1d8544f21c92df063
Michael Lambert <mlamber7@its.jnj.com>
2014-06-08 22:04:54 -0400

    simplified error structure

--- user-data/service/src/main/java/com/healthmedia/ws/common/error/v1/InvalidQueryExceptionMapper.java
+++ user-data/service/src/main/java/com/healthmedia/ws/common/error/v1/InvalidQueryExceptionMapper.java
@@ -14,12 +14,11 @@ public class InvalidQueryExceptionMapper extends AbstractErrorV1ExceptionMapper<
 	
 	@Override
 	public Response toResponse(InvalidQueryException exception) {
-
-		ErrorType error = new ErrorType();
 		
+		ErrorType error = new ErrorType();
 		error.setCode("INVALID_QUERY");
 		error.setName("Invalid Query");
-
+		
 		return Response.status(Response.Status.BAD_REQUEST).entity(error).type(super.getMediaType()).build();
 	}
 }
