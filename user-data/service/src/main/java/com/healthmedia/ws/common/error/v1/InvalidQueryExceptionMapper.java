0194345f397a2557b5b59cb75cbd1170a78ad8a9
Michael Lambert <mlamber7@its.jnj.com>
2014-06-11 15:52:12 -0400

    leverage soap fault for rest error message

--- user-data/service/src/main/java/com/healthmedia/ws/common/error/v1/InvalidQueryExceptionMapper.java
+++ user-data/service/src/main/java/com/healthmedia/ws/common/error/v1/InvalidQueryExceptionMapper.java
@@ -2,11 +2,16 @@ package com.healthmedia.ws.common.error.v1;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+import javax.xml.namespace.QName;
 
-import com.healthmedia.ws.common.error.v1.ErrorType;
+import org.w3.soap.envelop.v1.Fault;
+import org.w3.soap.envelop.v1.Faultcode;
+import org.w3.soap.envelop.v1.Faultreason;
+import org.w3.soap.envelop.v1.Reasontext;
+import org.w3.soap.envelop.v1.Subcode;
 
 /**
- * Example exception mapper
+ * Example exception mapper. Maps to a SOAP fault object defined in our xsd.
  *
  */
 @Provider
@@ -15,9 +20,31 @@ public class InvalidQueryExceptionMapper extends AbstractErrorV1ExceptionMapper<
 	@Override
 	public Response toResponse(InvalidQueryException exception) {
 		
-		ErrorType error = new ErrorType();
-		error.setCode("INVALID_QUERY");
-		error.setName("Invalid Query");
+		Fault error = new Fault();
+		//
+		// the client has created the problem
+		//
+		Faultcode code = new Faultcode();
+		code.setValue(new QName("", "Sender"));
+		//
+		// this is the actual error
+		//
+		Subcode subcode = new Subcode();
+		subcode.setValue(new QName("", "BAD_QUERY"));
+		code.setSubcode(subcode);
+		
+		error.setCode(code);
+		//
+		// description of issue in human readable text
+		//
+		Faultreason reason = new Faultreason();
+		Reasontext text = new Reasontext();
+		text.setValue("Bad query yo!");
+		text.setLang("en-US");
+		
+		reason.getText().add(text);
+		
+		error.setReason(reason);
 		
 		return Response.status(Response.Status.BAD_REQUEST).entity(error).type(this.getMediaType()).build();
 	}
