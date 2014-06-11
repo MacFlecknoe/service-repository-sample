185834bbb6e7ed9ab73d7051d20ea56dd0a7cb92
Michael Lambert <mlamber7@its.jnj.com>
2014-06-11 19:29:36 -0400

    leverage soap fault for rest error message

--- user-data/service/src/main/java/com/healthmedia/ws/common/error/v1/InvalidQueryExceptionMapper.java
+++ user-data/service/src/main/java/com/healthmedia/ws/common/error/v1/InvalidQueryExceptionMapper.java
@@ -1,9 +1,13 @@
 package com.healthmedia.ws.common.error.v1;
 
+import java.util.Locale;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.namespace.QName;
 
+import org.w3.soap.envelop.v1.Detail;
 import org.w3.soap.envelop.v1.Fault;
 import org.w3.soap.envelop.v1.Faultcode;
 import org.w3.soap.envelop.v1.Faultreason;
@@ -12,14 +16,14 @@ import org.w3.soap.envelop.v1.Subcode;
 
 /**
  * Example exception mapper. Maps to a SOAP fault object defined in our xsd.
- *
+ * 
  */
 @Provider
 public class InvalidQueryExceptionMapper extends AbstractErrorV1ExceptionMapper<InvalidQueryException> {
-	
+
 	@Override
 	public Response toResponse(InvalidQueryException exception) {
-		
+
 		Fault error = new Fault();
 		//
 		// the client has created the problem
@@ -30,22 +34,63 @@ public class InvalidQueryExceptionMapper extends AbstractErrorV1ExceptionMapper<
 		// this is the actual error
 		//
 		Subcode subcode = new Subcode();
-		subcode.setValue(new QName("", "BAD_QUERY"));
+		subcode.setValue(new QName("", "BadArguments"));
 		code.setSubcode(subcode);
-		
+
 		error.setCode(code);
 		//
 		// description of issue in human readable text
 		//
 		Faultreason reason = new Faultreason();
 		Reasontext text = new Reasontext();
-		text.setValue("Bad query yo!");
-		text.setLang("en-US");
-		
+		text.setValue("Invalid Query Parameter");
+		text.setLang(Locale.US.getLanguage());
+
 		reason.getText().add(text);
-		
+
 		error.setReason(reason);
+		//
+		// application specific error message
+		//
+		Detail detail = new Detail();
+		detail.getAny().add(new InvalidParameterType("query", exception.getQuery()));
+		detail.getAny().add(new InvalidParameterType("query", exception.getQuery()));
 		
+		error.setDetail(detail);
+
 		return Response.status(Response.Status.BAD_REQUEST).entity(error).type(this.getMediaType()).build();
+	}
+
+	@XmlRootElement(name="invalidParameter")
+	public static class InvalidParameterType {
+
+		protected String name;
+		protected String value;
+		
+		public InvalidParameterType() {
+			// empty
+		}
+		
+		public InvalidParameterType(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String value) {
+			this.name = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
 	}
 }
