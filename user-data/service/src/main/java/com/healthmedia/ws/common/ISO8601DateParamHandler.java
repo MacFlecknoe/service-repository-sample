2c174ad8476ad9701c621f1af0af869edd8dab1c
Michael Lambert <michael@michaellambert.com>
2014-06-25 13:38:44 -0700

    added iso 8602 support for dates

--- user-data/service/src/main/java/com/healthmedia/ws/common/ISO8601DateParamHandler.java
+++ user-data/service/src/main/java/com/healthmedia/ws/common/ISO8601DateParamHandler.java
@@ -5,11 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.cxf.jaxrs.ext.ParameterHandler;
-import org.apache.log4j.Logger;
 
 public class ISO8601DateParamHandler implements ParameterHandler<Date> {
-
-	private static Logger LOGGER = Logger.getLogger(ISO8601DateParamHandler.class);
 	
 	@Override
 	public Date fromString(String string) {
