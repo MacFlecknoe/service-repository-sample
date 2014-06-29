f7c6ae1d1aff075cbb54ddfac46fa4dbfc23b622
Michael Lambert <michael@michaellambert.com>
2014-06-28 18:16:59 -0700

    adding user service stubs

---
+++ user/service/src/main/java/com/healthmedia/ws/wsdl/user/v1/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.healthmedia.ws.wsdl.user.v1;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public AddUserResponse addUser(AddUserRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddUserDataResponse addUserData(AddUserDataRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
