c681109b2fbc7b0adb5863b40f9fc37096beb905
Michael Lambert <michael@michaellambert.com>
2014-06-28 18:55:39 -0700

    add mock return value

--- user/service/src/main/java/com/healthmedia/ws/wsdl/user/v1/UserServiceImpl.java
+++ user/service/src/main/java/com/healthmedia/ws/wsdl/user/v1/UserServiceImpl.java
@@ -1,11 +1,43 @@
 package com.healthmedia.ws.wsdl.user.v1;
 
+import com.healthmedia.ws.entity.dataSouce.v1.DataSourceType;
+import com.healthmedia.ws.entity.user.v1.DataType;
+import com.healthmedia.ws.entity.user.v1.UserDataCollectionType;
+import com.healthmedia.ws.entity.user.v1.UserDataType;
+import com.healthmedia.ws.entity.user.v1.UserType;
+
 public class UserServiceImpl implements UserService {
 
 	@Override
 	public AddUserResponse addUser(AddUserRequest parameters) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		UserType user = new UserType();
+		
+		user.setId("1");
+		user.setFirstName("Michael");
+		user.setLastName("Lambert");
+		UserDataCollectionType userDataCollection = user.getUserDataCollection();
+		userDataCollection.getUserData().add(null);
+		
+		UserType userReference = new UserType();
+		userReference.setId("1");
+		
+		DataSourceType sourceReference = new DataSourceType();
+		
+		DataType data = new DataType();
+		data.setName("WeightPounds");
+		data.setValue("145");
+		
+		UserDataType userData = new UserDataType();
+		userData.setId("a");
+		userData.setUser(userReference);
+		userData.setDataSource(sourceReference);
+		userData.setData(data);
+		
+		AddUserResponse response = new AddUserResponse();
+		response.setUser(user);
+		
+		return response;
 	}
 
 	@Override
