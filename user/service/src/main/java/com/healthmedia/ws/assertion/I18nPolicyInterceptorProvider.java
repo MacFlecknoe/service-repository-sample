cfbf53c7f7d3c65b08e3775f6931b046b631aa5e
Michael Lambert <michael@michaellambert.com>
2014-07-02 16:17:01 -0700

    changed name of policy interceptor proider to more accurately reflect function

--- user/service/src/main/java/com/healthmedia/ws/assertion/I18nPolicyInterceptorProvider.java
+++ user/service/src/main/java/com/healthmedia/ws/assertion/I18nPolicyInterceptorProvider.java
@@ -7,19 +7,14 @@ import javax.xml.namespace.QName;
 
 import org.apache.cxf.ws.policy.AbstractPolicyInterceptorProvider;
 
-public class CustomPolicyInterceptorProvider extends AbstractPolicyInterceptorProvider {
+public class I18nPolicyInterceptorProvider extends AbstractPolicyInterceptorProvider {
 
 	private static final long serialVersionUID = 1L;
 	
 	private static final Collection<QName> ASSERTION_TYPES = Arrays.asList(I18nHeaderInterceptor.ASSERTION);
 	
-	public CustomPolicyInterceptorProvider(Collection<QName> at) {
-		super(at);
-	}
-	
-	public CustomPolicyInterceptorProvider() {
+	public I18nPolicyInterceptorProvider() {
 		super(ASSERTION_TYPES);
 		getInInterceptors().add(new SpringI18nHeaderInterceptor());
 	}
-
 }
