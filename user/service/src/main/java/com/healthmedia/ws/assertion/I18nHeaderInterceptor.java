96f7fa83eb0d2a3bf37ce38803f0439aeae28214
Michael Lambert <michael@michaellambert.com>
2014-07-02 18:13:44 -0700

    removed jaxb dependencies from i18n policy interceptor

--- user/service/src/main/java/com/healthmedia/ws/assertion/I18nHeaderInterceptor.java
+++ user/service/src/main/java/com/healthmedia/ws/assertion/I18nHeaderInterceptor.java
@@ -3,28 +3,25 @@ package com.healthmedia.ws.assertion;
 import java.util.Collection;
 
 import javax.xml.namespace.QName;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.cxf.binding.soap.SoapMessage;
-import org.apache.cxf.databinding.DataReader;
 import org.apache.cxf.headers.Header;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
-import org.apache.cxf.service.Service;
-import org.apache.cxf.service.model.ServiceModelUtil;
 import org.apache.cxf.ws.policy.AssertionInfo;
 import org.apache.cxf.ws.policy.AssertionInfoMap;
 import org.apache.neethi.Assertion;
-import org.w3._2005._09.ws_i18n.International;
 import org.w3c.dom.Node;
 
 /**
  * <p>Adds support for Web Services Internationalization (WS-I18N). This implementation is meant to accept the specific locale and language preference(s) of the 
  * requester and act upon it. In this specific case the default locale of the requester is set to that of the international language node in the passed 
  * ws-i18n header.</p>
- * 
- * <p>In order for this interceptor to work it must be able to unmarshall ws-i18n elements; for this to happen {@link org.w3._2005._09.ws_i18n.International} needs to be 
- * included in the server's JAXB context.</p>
  * 
  * @see http://www.w3.org/TR/2012/NOTE-ws-i18n-20120522/
  * @author mlamber7
@@ -35,31 +32,47 @@ public class I18nHeaderInterceptor extends AbstractPhaseInterceptor<SoapMessage>
 	public static final QName WS_I18N_POLICY = new QName("http://www.w3.org/2008/04/ws-i18np", "i18n");
 	private static final QName WS_I18N_HEADER = new QName("http://www.w3.org/2005/09/ws-i18n", "international");
 	
+	private final XPathExpression localeExpression;
+	private final XPathExpression timeZoneExpression;
+	
 	private final ILocaleHandler handler;
 	
 	public I18nHeaderInterceptor(ILocaleHandler handler) {
 		super(Phase.PRE_INVOKE);
-		this.handler = handler;
+		try {
+			this.handler = handler;
+			
+			XPathFactory factory = XPathFactory.newInstance();
+			
+			this.localeExpression = factory.newXPath().compile("//*[local-name()='international']/*[local-name()='locale']/text()");
+			this.timeZoneExpression = factory.newXPath().compile("//*[local-name()='international']/*[local-name()='tz']/text()");
+			
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	@Override
 	public void handleMessage(SoapMessage message) throws Fault {
-		
-		Header header = message.getHeader(WS_I18N_HEADER);
-		
-		if(header != null) {
-			
-			Service service = ServiceModelUtil.getService(message.getExchange());
-			DataReader<Node> dataReader = service.getDataBinding().createReader(Node.class);
-			
-			International i18nHeader = (International)dataReader.read(WS_I18N_HEADER, (Node)header.getObject(), International.class);
-			message.setContextualProperty(International.class.getName(), i18nHeader);
 
-			handler.setLocale(i18nHeader);
+		try {
+			Header header = message.getHeader(WS_I18N_HEADER);
 			
-			handleAssertion(message, true);
-		} else {
-			handleAssertion(message, false);
+			if(header != null) {
+				
+				Node node = (Node)header.getObject();
+				
+				String locale = (String) localeExpression.evaluate(node,  XPathConstants.STRING);
+				String timeZone = (String) (String) timeZoneExpression.evaluate(node,  XPathConstants.STRING);
+				
+				handler.setLocale(locale);
+				handleAssertion(message, true);
+				
+			} else {
+				handleAssertion(message, false);
+			}
+		} catch (XPathExpressionException e) {
+			throw new RuntimeException(e);
 		}
 	}
 	
@@ -82,6 +95,6 @@ public class I18nHeaderInterceptor extends AbstractPhaseInterceptor<SoapMessage>
 	}
 	
 	public static interface ILocaleHandler {
-		public void setLocale(International i18nLocale);
+		public void setLocale(String i18nLocale);
 	}
 }
--- user/service/src/main/java/com/healthmedia/ws/assertion/SpringLocaleHandler.java
+++ user/service/src/main/java/com/healthmedia/ws/assertion/SpringLocaleHandler.java
@@ -4,14 +4,11 @@ import java.util.Locale;
 
 import org.apache.cxf.common.util.StringUtils;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.w3._2005._09.ws_i18n.International;
 
 public class SpringLocaleHandler implements I18nHeaderInterceptor.ILocaleHandler {
 
 	@Override
-	public void setLocale(International international) {
-		
-		String i18nLocale = international.getLocale();
+	public void setLocale(String i18nLocale) {
 		
 		if(!StringUtils.isEmpty(i18nLocale)) {
 			Locale locale = new Locale(i18nLocale);
