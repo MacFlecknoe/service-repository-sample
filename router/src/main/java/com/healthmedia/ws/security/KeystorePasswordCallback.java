d6579c58356eca07fe91ae1ef076d7028f526eeb
Michael Lambert <michael@michaellambert.com>
2014-07-06 12:27:37 -0700

    slight refacotr and bug squashing

---
+++ router/src/main/java/com/healthmedia/ws/security/KeystorePasswordCallback.java
@@ -0,0 +1,44 @@
+package com.healthmedia.ws.security;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.ws.security.WSPasswordCallback;
+
+/**
+ * Handles passwords for the private keys in keystore.
+ * 
+ * @author mlamber7
+ *
+ */
+public class KeystorePasswordCallback implements CallbackHandler {
+
+	private Map<String, String> passwords;
+
+	public KeystorePasswordCallback(Map<String, String> passwords) {
+		this.passwords = passwords;
+	}
+	
+	public KeystorePasswordCallback() {
+		this(Collections.singletonMap("serveralias", "password"));
+	}
+	
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		for (int i = 0; i < callbacks.length; i++) {
+			WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
+
+			String pass = passwords.get(pc.getIdentifier());
+			
+			if (pass != null) {
+				pc.setPassword(pass);
+				return;
+			}
+		}
+	}
+}
