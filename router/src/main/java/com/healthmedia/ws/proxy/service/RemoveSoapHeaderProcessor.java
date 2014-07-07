5d1201472ccca716f48a09e2782de3b9af2e22e7
Michael Lambert <michael@michaellambert.com>
2014-07-06 19:10:19 -0700

    fully working x509

---
+++ router/src/main/java/com/healthmedia/ws/proxy/service/RemoveSoapHeaderProcessor.java
@@ -0,0 +1,43 @@
+package com.healthmedia.ws.proxy.service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.util.CastUtils;
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.headers.Header;
+
+/**
+ * We need to remove the SOAP headers we have processed in the proxy.
+ * 
+ * @author mlamber7
+ *
+ */
+public class RemoveSoapHeaderProcessor implements Processor {
+	
+	private final Collection<QName> qnames;
+	
+	public RemoveSoapHeaderProcessor(QName qname) {
+		this(Collections.singletonList(qname));
+	}
+	
+	public RemoveSoapHeaderProcessor(Collection<QName> qnames) {
+		this.qnames = qnames;
+	}
+	
+	public void process(Exchange exchange) throws Exception {
+		
+		List<SoapHeader> soapHeaders = CastUtils.cast((List<?>) exchange.getIn().getHeader(Header.HEADER_LIST)); // org.apache.cxf.headers.header.list
+		
+		for(SoapHeader header : soapHeaders) {
+			if(qnames.contains(header.getName())){
+				soapHeaders.remove(header);
+			}
+		}
+	}
+}
--- router/src/main/java/com/healthmedia/ws/security/KeystorePasswordCallback.java
+++ router/src/main/java/com/healthmedia/ws/security/KeystorePasswordCallback.java
@@ -2,7 +2,6 @@ package com.healthmedia.ws.security;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.security.auth.callback.Callback;
@@ -12,7 +11,7 @@ import javax.security.auth.callback.UnsupportedCallbackException;
 import org.apache.ws.security.WSPasswordCallback;
 
 /**
- * Handles passwords for the private keys in keystore.
+ * Looks up passwords for the private keys placed in a keystore.
  * 
  * @author mlamber7
  *
@@ -33,7 +32,7 @@ public class KeystorePasswordCallback implements CallbackHandler {
 		for (int i = 0; i < callbacks.length; i++) {
 			WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
 
-			String pass = passwords.get(pc.getIdentifier());
+			String pass = passwords.get(pc.getIdentifier().toLowerCase());
 			
 			if (pass != null) {
 				pc.setPassword(pass);
