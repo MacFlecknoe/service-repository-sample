dfd7869668f5abd8d174032c1a564f78fa03db70
Michael Lambert <michael@michaellambert.com>
2014-08-27 10:56:22 -0700

    refactored user-data service to be user service

--- user-data/service/src/main/java/com/healthmedia/ws/service/rest/userdata/v1/UserDataServiceImpl.java
+++ user-data/service/src/main/java/com/healthmedia/ws/service/rest/userdata/v1/UserDataServiceImpl.java
@@ -15,9 +15,9 @@ import com.healthmedia.ws.common.error.BadArgumentsException.BadArgumentError;
 import com.healthmedia.ws.common.error.DataValidationException;
 import com.healthmedia.ws.common.error.DataValidationException.DataValidationError;
 import com.healthmedia.ws.common.error.GenericErrorException;
-import com.healthmedia.ws.common.v1.ModalityType;
 import com.healthmedia.ws.entity.dataSouce.v1.DataSourceType;
 import com.healthmedia.ws.entity.user.v1.DataType;
+import com.healthmedia.ws.entity.user.v1.UserCollectionType;
 import com.healthmedia.ws.entity.user.v1.UserDataCollectionType;
 import com.healthmedia.ws.entity.user.v1.UserDataType;
 import com.healthmedia.ws.entity.user.v1.UserType;
@@ -28,16 +28,74 @@ import com.healthmedia.ws.entity.user.v1.UserType;
  * @author mlamber7
  *
  */
-public class UserDataServiceImpl implements UserDataService {
+public class UserDataServiceImpl implements UserService {
 	
 	private static Logger LOGGER = Logger.getLogger(UserDataServiceImpl.class);
 	
 	private com.healthmedia.ws.entity.v1.ObjectFactory entityFactory = new com.healthmedia.ws.entity.v1.ObjectFactory();
 	private com.healthmedia.ws.entity.user.v1.ObjectFactory userFactory = new com.healthmedia.ws.entity.user.v1.ObjectFactory();
 	private com.healthmedia.ws.entity.dataSouce.v1.ObjectFactory dataSourceFactory = new com.healthmedia.ws.entity.dataSouce.v1.ObjectFactory();
-	
+
 	@Override
-	public UserDataType create(UserDataType userdatatype) {
+	public UserCollectionType findUserByQuery(String query, Date updateDate_start, Date updateDate_end) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public UserType createUser(UserType usertype) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public UserCollectionType createUsers(UserCollectionType usercollectiontype) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public UserType findUserById(String id) {
+		throw new UnsupportedOperationException();
+	}
+
+
+	@Override
+	public UserDataCollectionType findUserDatasByQuery(String userId, String query, Date updateDate_start, Date updateDate_end, List<String> data_name, List<String> user_id, UserDataCollectionType userdatacollectiontype) {
+		if(LOGGER.isDebugEnabled()) {
+			LOGGER.debug("executing find by query");
+		}
+		if(query == null || query.equals("bad_query")) {
+			
+			BadArgumentsException badArgumentException = new BadArgumentsException();
+			
+			badArgumentException.getBadArgumentErrors().add(new BadArgumentError("query", query));
+			badArgumentException.getBadArgumentErrors().add(new BadArgumentError("updateDate_end", updateDate_start)); 
+			
+			throw badArgumentException;
+		}
+		if(query.equals("data_def")) {
+			
+			DataValidationException validationException = new DataValidationException("123", "validation issue");
+			
+			validationException.getDataValidationErrors().add(new DataValidationError("ABC", "Foo", "This is foo"));
+			validationException.getDataValidationErrors().add(new DataValidationError("DEF", "Bar", "This is bar"));
+			
+			throw validationException;
+		}
+		if(query.equals("error_code")) {
+			throw new GenericErrorException("1234CODE", "Generic error message");
+		}
+		if(query.equals("throwable")) {
+			throw new RuntimeException("this should be logged");
+		}
+		UserDataCollectionType collection = new UserDataCollectionType();
+		
+		collection.getUserData().add(this.findUserDataById(userId, "342a"));
+		collection.getUserData().add(this.findUserDataById(userId, "34234a"));
+		
+		return collection;
+	}
+
+	@Override
+	public void createUserData(String userId, UserDataType userdatatype) {
 		if(LOGGER.isDebugEnabled()) {
 			LOGGER.debug("executing create");
 		}
@@ -55,17 +113,30 @@ public class UserDataServiceImpl implements UserDataService {
 			userData.setCreateDate(entityFactory.createEntityTypeCreateDate(today));
 			userData.setDataSource(userdatatype.getDataSource());
 			userData.setId(entityFactory.createEntityTypeId("23"));
-			userData.setModality(userFactory.createUserDataTypeModality(ModalityType.WEB));
 			
-			return userData;
+			//return userData;
 		
 		} catch (DatatypeConfigurationException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	@Override
-	public UserDataType findById(String id) {
+	public void createUserDatas(String userId, UserDataCollectionType userdatacollectiontype) {
+		if(LOGGER.isDebugEnabled()) {
+			LOGGER.debug("executing create collection");
+		}
+		List<UserDataType> userDataTypes = userdatacollectiontype.getUserData();
+		
+		for(UserDataType userData : userDataTypes) {
+			createUserData(userId, userData);
+		}
+		//return userdatacollectiontype;
+		
+	}
+
+	@Override
+	public UserDataType findUserDataById(String userId, String userDataId) {
 		if(LOGGER.isDebugEnabled()) {
 			LOGGER.debug("executing find by id");
 		}			
@@ -98,62 +169,11 @@ public class UserDataServiceImpl implements UserDataService {
 			userData.setData(userFactory.createUserDataTypeData(data));
 			userData.setUpdateDate(entityFactory.createEntityTypeUpdateDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(updateDate)));
 			userData.setCreateDate(entityFactory.createEntityTypeCreateDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(createDate)));
-			userData.setModality(userFactory.createUserDataTypeModality(ModalityType.WEB));
 			
 			return userData;
 			
 		} catch (DatatypeConfigurationException e) {
 			throw new RuntimeException(e);
 		}
-	}
-	
-	@Override
-	public UserDataCollectionType findByQuery(String query, Date updateDate_start, Date updateDate_end, List<String> data_name, List<String> user_id) {
-		if(LOGGER.isDebugEnabled()) {
-			LOGGER.debug("executing find by query");
-		}
-		if(query == null || query.equals("bad_query")) {
-			
-			BadArgumentsException badArgumentException = new BadArgumentsException();
-			
-			badArgumentException.getBadArgumentErrors().add(new BadArgumentError("query", query));
-			badArgumentException.getBadArgumentErrors().add(new BadArgumentError("updateDate_end", updateDate_start)); 
-			
-			throw badArgumentException;
-		}
-		if(query.equals("data_def")) {
-			
-			DataValidationException validationException = new DataValidationException("123", "validation issue");
-			
-			validationException.getDataValidationErrors().add(new DataValidationError("ABC", "Foo", "This is foo"));
-			validationException.getDataValidationErrors().add(new DataValidationError("DEF", "Bar", "This is bar"));
-			
-			throw validationException;
-		}
-		if(query.equals("error_code")) {
-			throw new GenericErrorException("1234CODE", "Generic error message");
-		}
-		if(query.equals("throwable")) {
-			throw new RuntimeException("this should be logged");
-		}
-		UserDataCollectionType collection = new UserDataCollectionType();
-		
-		collection.getUserData().add(this.findById("342a"));
-		collection.getUserData().add(this.findById("34234a"));
-		
-		return collection;
-	}
-
-	@Override
-	public UserDataCollectionType createCollection(UserDataCollectionType userdatacollectiontype) {
-		if(LOGGER.isDebugEnabled()) {
-			LOGGER.debug("executing create collection");
-		}
-		List<UserDataType> userDataTypes = userdatacollectiontype.getUserData();
-		
-		for(UserDataType userData : userDataTypes) {
-			create(userData);
-		}
-		return userdatacollectiontype;
 	}
 }
