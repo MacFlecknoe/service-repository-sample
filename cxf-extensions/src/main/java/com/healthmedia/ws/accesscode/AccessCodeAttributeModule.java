1bc94b11695bf6223ca35000d581b4310187f450
Michael Lambert <michael@michaellambert.com>
2014-09-22 13:19:53 -0700

    added attribute locator

---
+++ cxf-extensions/src/main/java/com/healthmedia/ws/accesscode/AccessCodeAttributeModule.java
@@ -0,0 +1,93 @@
+package com.healthmedia.ws.accesscode;
+
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jboss.security.xacml.locators.AttributeLocator;
+import org.jboss.security.xacml.sunxacml.EvaluationCtx;
+import org.jboss.security.xacml.sunxacml.attr.AttributeDesignator;
+import org.jboss.security.xacml.sunxacml.attr.BagAttribute;
+import org.jboss.security.xacml.sunxacml.attr.StringAttribute;
+import org.jboss.security.xacml.sunxacml.cond.EvaluationResult;
+
+public class AccessCodeAttributeModule extends AttributeLocator {
+
+	private static final String ACCESS_CODE_IDENTIFIER= "urn:healthmedia:names:subject:access-code:v1";
+	
+	private final IAccessCodeFinder finder;
+	
+	public AccessCodeAttributeModule(IAccessCodeFinder finder) {
+		this.finder = finder;
+	}
+	
+	public AccessCodeAttributeModule() {
+		this(new MockAccessCodeFinder());
+	}
+	
+	@Override
+	public boolean isDesignatorSupported() {
+		// always return true, since this is a feature we always support
+		return true;
+	}
+
+	@Override
+	public Set<Integer> getSupportedDesignatorTypes() {
+		//
+		// return a single identifier that shows support for environment attrs
+		//
+		Set<Integer> set = new HashSet<Integer>();
+		set.add(new Integer(AttributeDesignator.SUBJECT_TARGET));
+
+		return set;
+	}
+
+	@Override
+	public EvaluationResult findAttribute(
+			URI attributeType, 
+			URI attributeId,
+			URI issuer, 
+			URI subjectCategory, 
+			EvaluationCtx context,
+			int designatorType) {
+		
+		if (designatorType != AttributeDesignator.SUBJECT_TARGET) {
+			// make sure this is an Subject attribute
+			return new EvaluationResult(BagAttribute.createEmptyBag(attributeType));
+		}
+		if (!attributeId.toString().equals(ACCESS_CODE_IDENTIFIER)) {
+			// make sure they're asking for our identifier
+			return new EvaluationResult(BagAttribute.createEmptyBag(attributeType));
+		}
+		if (!attributeType.toString().equals(StringAttribute.identifier)) {
+			// make sure they're asking for an string return value
+			return new EvaluationResult(BagAttribute.createEmptyBag(attributeType));
+		}
+		// now that everything checks out, get the access-code list...
+		Set<StringAttribute> accessCodeSet = new HashSet<StringAttribute>();
+		
+		for(String accessCode : finder.getAcccessCodes("test")) {
+			accessCodeSet.add(new StringAttribute(accessCode));
+		}
+		return new EvaluationResult(new BagAttribute(attributeType, accessCodeSet));
+	}
+	
+	public static interface IAccessCodeFinder {
+		public Set<String> getAcccessCodes(String username);
+	}
+	
+	public static class MockAccessCodeFinder implements IAccessCodeFinder {
+
+		@Override
+		public Set<String> getAcccessCodes(String username) {
+			
+			Set<String> accessCodeSet = new HashSet<String>();
+			
+			accessCodeSet.add("testAccessCode");
+			accessCodeSet.add("anotherTestAccessCode");
+			accessCodeSet.add("yetAnotherTestAccessCode");
+			
+			return accessCodeSet;
+		}
+	}
+}
