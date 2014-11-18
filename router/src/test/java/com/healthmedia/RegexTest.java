c729739adafb2938054f25ae8d23edbd1bda3584
Michael Lambert <michael@michaellambert.com>
2014-11-18 06:23:36 -0800

    adding transformation for namespace

--- router/src/test/java/com/healthmedia/RegexTest.java
+++ router/src/test/java/com/healthmedia/RegexTest.java
@@ -10,13 +10,9 @@ public class RegexTest {
 	public void test() {
 		
 		String test = "\"hello.dd\"";
-		
-		System.out.println(test);
-		
-		//System.out.println(test.replaceAll("\"[\\w][\\w]+\"", "_"));
 		System.out.println(test.replaceAll("\"([A-Za-z][\\w]+)\\.(\\w)+\"", "$1_$2"));
 		
-		fail("Not yet implemented");
+		assertTrue(true);
 	}
 
 }
